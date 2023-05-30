d842c0e03aa9f565e3ec528fb37c813d4f1f16a0
constdrop <hirota@dogrun.jp>
2023-05-30 23:07:44 +0900

    Generate gem configuration with bundle gem.

---
+++ lib/nokogiri/enumparse.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "enumparse/version"
+
+module Nokogiri
+  module Enumparse
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
